6fa4c5045636a7b808effb41ad97b1a5da067726
Kristina Gorina <kristina.gorina@kristina.gorina-MacBook-Pro>
2022-12-12 16:47:39 -0500

    Created endpoint to get specific user

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {User} = require('../models/index');
 
-//  /users returns all users
+//---/users returns all users
 router.get('/',async(req,res)=>{
     res.json(await User.findAll())
 })
+//---/users/id returns user with that id
 router.get("/:id", async(req,res)=>{
+    res.json(await User.findByPk(req.params.id))
+})
+//---/users/~id~/shows returns user's shows
+router.get("/:id/shows", async(req,res)=>{
     
 })
 module.exports = router;
